ca12d979315df8d7955a3a8aa6949d3588736fcd
Sebastian Holmes <skerlec7693@gmail.com>
2020-04-04 11:37:22 -0700

    linked js file to html and modified js file

---
+++ index.js
@@ -0,0 +1,29 @@
+$( function() {
+
+    $( "#js-shopping-list-form" ).on( "submit", function( event ) { 
+        let text = $( "#shopping-list-entry" ).val();
+        $( "ul" ).append( 
+        `<li>
+            <span class="shopping-item">${text}</span>
+            <div class="shopping-item-controls">
+                <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                </button>
+                <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                </button>
+            </div>
+        </li>`);
+        $( "#shopping-list-entry" ).val("");
+        event.preventDefault();
+    });
+
+    $( ".shopping-list" ).on( "click", ".shopping-item-toggle", function( event ) {
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+    });
+
+    $( ".shopping-list" ).on( "click", ".shopping-item-delete", function( event ) {
+        this.closest( "li" ).remove();
+    });
+
+});
